7f26e12822322a854f9b47e2763cfe84ebbba87c
zikirzodafaridun <f62710546@gmail.com>
2025-01-19 13:59:12 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files (use python3.9 or python depending on your setup)
-python3.9 manage.py collectstatic
+# Run Django collectstatic command
+python manage.py collectstatic --noinput
