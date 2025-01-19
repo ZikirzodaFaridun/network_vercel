00879ae38ad82aef05c696e78a591601ca06cdc1
zikirzodafaridun <f62710546@gmail.com>
2025-01-19 13:45:17 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt 
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files (use python3.9 or python depending on your setup)
 python3.9 manage.py collectstatic
